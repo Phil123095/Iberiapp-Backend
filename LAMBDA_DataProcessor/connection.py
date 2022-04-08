295d89b6c2735f2a5dd56b35a85709b5f688f215
Phil123095 <68194774+Phil123095@users.noreply.github.com>
2022-04-08 16:19:37 +0200

    apply secrets to lambda connection file

--- LAMBDA_DataProcessor/connection.py
+++ LAMBDA_DataProcessor/connection.py
@@ -1,17 +1,26 @@
 from sqlalchemy import create_engine, engine
+import os
+import dotenv
 
 
 def get_db_connections():
+    local = True
+    if local:
+        dotenv.load_dotenv()
+
+    db_user = os.environ['DB_USER']
+    password = os.environ['DB_PASS']
+    db_host = os.environ['DB_HOST']
+    df_name = os.environ['DB_NAME']
+
     engine_final = create_engine(
-        # Equivalent URL:
-        # mysql+pymysql://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
         engine.url.URL.create(
             drivername="mysql+mysqlconnector",
-            username='admin',  # e.g. "my-database-user"
-            password='blockchaintest1',  # e.g. "my-database-password"
-            host='blockchain-db.c1gc1t7hjfvr.eu-central-1.rds.amazonaws.com',  # e.g. "127.0.0.1"
+            username=db_user,  # e.g. "my-database-user"
+            password=password,  # e.g. "my-database-password"
+            host=db_host,  # e.g. "127.0.0.1"
             port=3306,  # e.g. 3306
-            database='TI_database'  # e.g. "my-database-name",
+            database=df_name  # e.g. "my-database-name",
         )
     )
     print("Connection established.")
